14932998e96f4143752a5034e18d3c48fabbc9b2
Shafaet1722298642 <93768009+Shafaet1722298642@users.noreply.github.com>
2022-03-22 11:45:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(i => {
+i.addEventListener('click',()=> {
+    removeActiveClasses()
+    i.classList.add('active')
+})
+})
+function removeActiveClasses(){
+    panels.forEach(i =>{
+        i.classList.remove('active')
+    })
+}
+ 
